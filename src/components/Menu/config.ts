dd26d8a19b1b163dc186df7016e41d8c2b8a07be
human <human@MacBook-Pro-2.local>
2021-05-30 14:02:11 +0530

    update

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -31,13 +31,55 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: 'https://app.kesef.finance/nests'
   },
-  {
+ {
     label: 'Info',
     icon: 'InfoIcon',
     items: [
       {
+        label: 'DexGuru',
+        href: 'https://dex.guru/token/0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28-bsc',
+      },
+      {
+        label: 'Poocoin',
+        href: 'https://poocoin.app/tokens/0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28',
+      },
+      {
+        label: 'BoggedFinance',
+        href: 'https://charts.bogged.finance/?token=0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28',
+      },
+      {
+        label: 'DexTools',
+        href: 'https://www.dextools.io/app/pancakeswap/pair-explorer/0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28',
+      },
+      {
         label: 'PancakeSwap',
         href: 'https://pancakeswap.info/token/0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28',
+      },
+    ],
+  },
+  {
+    label: 'Listings',
+    icon: 'GroupsIcon',
+    items: [
+      {
+        label: 'BscScan',
+        href: 'https://bscscan.com/token/0xf056c78AE863b08808c1FE3c0f8a7dB2a4FDcf28',
+      },
+      {
+        label: 'DappRadar',
+        href: 'https://dappradar.com/binance-smart-chain/defi/kesef-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/kesef-finance/',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/kesef-finance',
+      },
+      {
+        label: 'Vfat',
+        href: 'https://vfat.tools/bsc/kesef-finance',
       },
     ],
   },
@@ -49,12 +91,16 @@ const config: MenuEntry[] = [
         label: 'Github',
         href: 'https://github.com/kesef-finance/',
       },
+       {
+        label: 'Docs',
+        href: 'https://docs.kesef.finance',
+      },
       {
         label: 'Blog',
         href: 'https://kesef.medium.com/',
       },
     ],
-  },
+  }
 ]
 
 export default config
